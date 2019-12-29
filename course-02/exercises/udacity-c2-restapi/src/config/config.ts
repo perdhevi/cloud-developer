d700d32be2eaad370914c84d0fb64465cad61c3d
Raditya Perdhevi <raditya@perdhevi.com>
2019-12-29 15:17:48 +0700

    test config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "postgres",
+    "password": "5SGWkHB3grDv9hQD",
+    "database": "udagramperdhevidev",
+    "host": "udagramperdhevidev.cznbrnyd2yii.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-perdhevi-dev"
   },
   "prod": {
     "username": "",

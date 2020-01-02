7fe3217f3985bb1054b0d1e064d735b26236b399
Raditya Perdhevi <raditya@perdhevi.com>
2020-01-03 02:50:56 +0700

    password in env var

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,20 +1,19 @@
-import { internalconfig } from './internal';
 export const config = {
   "dev": {
-    "username": internalconfig.dev.username,
-    "password": internalconfig.dev.password,
-    "database": internalconfig.dev.database,
-    "host": internalconfig.dev.host,
-    "dialect": internalconfig.dev.dialect,
-    "aws_region": internalconfig.dev.aws_region,
-    "aws_profile": internalconfig.dev.aws_profile,
-    "aws_media_bucket": internalconfig.dev.aws_media_bucket
+    "username": process.env.POSTGRES_USERNAME,// internalconfig.dev.username,
+    "password": process.env.POSTGRES_PASSWORD, //internalconfig.dev.password,
+    "database": process.env.POSTGRES_DB, //internalconfig.dev.database,
+    "host": process.env.POSTGRES_HOST,//internalconfig.dev.host,
+    "dialect": process.env.POSTGRES_DIALECT,//internalconfig.dev.dialect,
+    "aws_region": process.env.POSTGRES_AWS_REGION,//internalconfig.dev.aws_region,
+    "aws_profile": process.env.AWS_PROFILE,//internalconfig.dev.aws_profile,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET//internalconfig.dev.aws_media_bucket
   },
   "prod": {
-    "username": internalconfig.prod.username,
-    "password": internalconfig.prod.password,
-    "database": internalconfig.prod.database,
-    "host": internalconfig.prod.host,
-    "dialect": internalconfig.prod.dialect
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": process.env.POSTGRES_DIALECT
   }
 }
